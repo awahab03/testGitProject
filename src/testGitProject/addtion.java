9f3f5b8b8d3c51aca0770117730c506734292b98
awahab03 <awahab03@yahoo.com>
2014-10-29 14:36:36 -0400

    initial commit for testGitProject

---
+++ src/testGitProject/addtion.java
@@ -0,0 +1,18 @@
+package testGitProject;
+
+public class addtion {
+
+	public static void main(String[] args) {
+		System.out.println("MY Addition method");
+		
+		System.out.println("Addition results= "+myAdditon(4,5));
+
+	}
+	
+	static int myAdditon(int x, int y){
+		int addition=x+y;
+		return addition;
+		
+	}
+
+}
